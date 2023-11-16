1c2b7e588ca7b5789130bd1c156cfc8649591fcc
Rustamov Kamronbek <kamronbekrustamov1@gmail.com>
2023-11-16 12:32:46 +0500

    Leetcode Implement Trie solution

---
+++ src/main/java/org/kamronbek/leetcode/medium/ImplementTrie.java
@@ -0,0 +1,61 @@
+package org.kamronbek.leetcode.medium;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImplementTrie {}
+class Trie {
+    private Node root;
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        if (word.isBlank()) {
+            throw new IllegalArgumentException("It is blank");
+        }
+        Node currentNode = this.root;
+        for (char c : word.toCharArray()) {
+            if (!currentNode.characterMap.containsKey(c)) {
+                currentNode.characterMap.put(c, new Node());
+            }
+            currentNode = currentNode.characterMap.get(c);
+        }
+        currentNode.isWord = true;
+    }
+
+    public boolean search(String word) {
+        if (word.isBlank()) {
+            throw new IllegalArgumentException("It is blank");
+        }
+        Node currentNode = this.root;
+        for (char c : word.toCharArray()) {
+            if (!currentNode.characterMap.containsKey(c)) {
+                return false;
+            }
+            currentNode = currentNode.characterMap.get(c);
+        }
+        return currentNode.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        if (prefix.isBlank()) {
+            throw new IllegalArgumentException("It is blank");
+        }
+        Node currentNode = this.root;
+        for (char c : prefix.toCharArray()) {
+            if (!currentNode.characterMap.containsKey(c)) {
+                return false;
+            }
+            currentNode = currentNode.characterMap.get(c);
+        }
+        return true;
+    }
+
+    private static class Node {
+        public Map<Character, Node> characterMap = new HashMap<>(26, 1);
+        public boolean isWord = false;
+    }
+}
+
