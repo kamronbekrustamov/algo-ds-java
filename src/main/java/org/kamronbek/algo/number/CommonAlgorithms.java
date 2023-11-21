24ab63393da43f04894c0afcdaf1cd25d7944f03
Rustamov Kamronbek <kamronbekrustamov1@gmail.com>
2023-11-22 00:13:45 +0500

    Add the algorithm to find the modular multiplicative inverse

--- src/main/java/org/kamronbek/algo/number/CommonAlgorithms.java
+++ src/main/java/org/kamronbek/algo/number/CommonAlgorithms.java
@@ -23,6 +23,38 @@ public class CommonAlgorithms {
         return first;
     }
 
+    // Find the modular multiplicative inverse using Extended Euclidean algorithm
+    public static long modularMultiplicativeInverse(long num, long mod) {
+        if (gcd(num, mod) != 1) {
+            throw new IllegalArgumentException("Numbers must be relatively prime");
+        }
+
+        // Making sure that a >= b
+        long a = mod;
+        long b = num % mod; // if the num is higher than mod, then making it less than mod
+        long quotient = a / b;
+        long remainder = a % b;
+        long t1 = 0;
+        long t2 = 1;
+        long t = t1 - t2 * quotient;
+
+        while (remainder != 0) {
+            a = b;
+            b = remainder;
+            quotient = a / b;
+            remainder = a % b;
+            t1 = t2;
+            t2 = t;
+            t = t1 - t2 * quotient;
+        }
+
+        // If the modular multiplicative inverse is negative, making it positive
+        while (t2 < 0) {
+            t2 += mod;
+        }
+        return t2;
+    }
+
     public static List<Long> primesLessThan(long upperBound) {
         return LongStream.range(2, upperBound).filter(CommonAlgorithms::isPrime).boxed().toList();
     }
