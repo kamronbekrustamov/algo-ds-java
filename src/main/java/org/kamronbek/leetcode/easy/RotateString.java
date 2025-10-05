6615d8fd3d7296c85fe27713e5e6e2e7dcdc59a1
Kamronbek Rustamov <kamronbekrustamov1@gmail.com>
2025-10-06 01:34:32 +0500

    Remove leetcode solutions as they are in different directory

--- src/main/java/org/kamronbek/leetcode/easy/RotateString.java
+++
@@ -1,7 +0,0 @@
-package org.kamronbek.leetcode.easy;
-
-public class RotateString {
-    public boolean rotateString(String A, String B) {
-        return A.length() == B.length() && (A + A).contains(B);
-    }
-}
